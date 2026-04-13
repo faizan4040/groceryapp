20e620bd9670834f8ef101668ca633b009dc496d
Faizan Saifi <Faizansaifi571@gmail.com>
2026-04-13 16:23:55 +0530

    new_updated_code

--- src/app/api/admin/coupons/[id]/route.ts
+++ src/app/api/admin/coupons/[id]/route.ts
@@ -1,24 +1,63 @@
 import connectDB from "@/lib/db";
 import couponsModel from "@/models/coupons.model";
-import { NextResponse } from "next/server";
-
+import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     await connectDB();
-    const { id } = params;
+
+    const { id } = await params;
 
     const deletedCoupon = await couponsModel.findByIdAndDelete(id);
 
     if (!deletedCoupon) {
-      return NextResponse.json({ message: "Coupon not found" }, { status: 404 });
+      return NextResponse.json(
+        { message: "Coupon not found" },
+        { status: 404 }
+      );
     }
 
-    return NextResponse.json({ message: "Coupon deleted successfully" }, { status: 200 });
+    return NextResponse.json(
+      { message: "Coupon deleted successfully" },
+      { status: 200 }
+    );
   } catch (error) {
-    return NextResponse.json({ message: "Delete failed" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Delete failed" },
+      { status: 500 }
+    );
   }
 }
+
+
+
+
+
+
+// import connectDB from "@/lib/db";
+// import couponsModel from "@/models/coupons.model";
+// import { NextResponse } from "next/server";
+
+
+// export async function DELETE(
+//   req: Request,
+//   { params }: { params: { id: string } }
+// ) {
+//   try {
+//     await connectDB();
+//     const { id } = params;
+
+//     const deletedCoupon = await couponsModel.findByIdAndDelete(id);
+
+//     if (!deletedCoupon) {
+//       return NextResponse.json({ message: "Coupon not found" }, { status: 404 });
+//     }
+
+//     return NextResponse.json({ message: "Coupon deleted successfully" }, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json({ message: "Delete failed" }, { status: 500 });
+//   }
+// }
